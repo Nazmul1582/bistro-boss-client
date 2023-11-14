5985b8dfe60420052b4d0a3a236e70b92329000c
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-14 21:17:31 +0600

    changed favicon and website title

--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -2,7 +2,7 @@ import image from "../../../assets/home/chef-service.jpg";
 const Cover = () => {
   return (
     <section
-      className="py-24 bg-cover"
+      className="py-24 bg-cover bg-fixed"
       style={{ backgroundImage: `url(${image})` }}
     >
       <div className="max-w-5xl mx-auto p-10 md:p-20 bg-white text-center">
