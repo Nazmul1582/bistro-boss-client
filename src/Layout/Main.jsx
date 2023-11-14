3876ea5e11a8e8953c2261d2e6c1358a36295f3b
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-14 06:51:08 +0600

    added header and footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom'
+import Footer from '../pages/Shared/Footer/Footer'
+import Header from '../pages/Shared/Header/Header'
 
 const Main = () => {
   return (
     <>
+        <Header />
         <Outlet />
+        <Footer />
     </>
   )
 }
