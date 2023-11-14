dab69e2c5cf7bbf3c86e619d783e776788ea107e
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-14 08:17:47 +0600

    created a re-useable component for section title

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,11 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const Category = () => {
+    return (
+        <section className='py-20'>
+            <SectionTitle subHeading="From 11:00am to 10:00pm" heading="Order Online" />
+        </section>
+    );
+};
+
+export default Category;
