63490e9e26311032b1b36944da3482dbb2175795
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-16 15:54:51 +0600

    added a pageCover in Menu page

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,10 +1,12 @@
 import Head from "../Shared/Head/Head";
+import PageCover from "../Shared/PageCover/PageCover";
+import image from '../../assets/menu/banner3.jpg'
 
 const Menu = () => {
     return (
         <section>
             <Head pageTitle="Menu" />
-            <p>Menu Page</p>
+            <PageCover img={image} title="Our Menu" />
         </section>
     );
 };
--- src/pages/Shared/PageCover/PageCover.jsx
+++ src/pages/Shared/PageCover/PageCover.jsx
@@ -8,16 +8,11 @@ const PageCover = ({ img, title }) => {
       strength={-200}
     >
       <section
-        className="py-24 bg-cover bg-fixed">
-        <div className="max-w-5xl mx-auto p-10 md:p-20 bg-black/50 text-neutral-content text-center">
-          <h2 className="text-5xl font-semibold mb-5">{title}</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Necessitatibus, libero accusamus laborum deserunt ratione dolor
-            officiis praesentium! Deserunt magni aperiam dolor eius dolore at,
-            nihil iusto ducimus incidunt quibusdam nemo.
-          </p>
-        </div>
+        className="h-[800px] grid place-items-center">
+          <div className="w-9/12 p-10 md:p-20 bg-black/50 text-neutral-content text-center">
+            <h2 className="text-4xl md:text-7xl font-bold mb-5">{title}</h2>
+            <p className="text-xl md:text-2xl">Would you like to try a dish?</p>
+          </div>
       </section>
     </Parallax>
   );
