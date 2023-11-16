8b5dd9c6240b235a002cc648f93a88c45a7ce282
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-16 20:40:25 +0600

    created a order page and added page banner

---
+++ src/pages/Order/Order.jsx
@@ -0,0 +1,12 @@
+import image from "../../assets/shop/banner2.jpg";
+import PageCover from "../Shared/PageCover/PageCover";
+
+const Order = () => {
+  return (
+    <>
+        <PageCover title="Order Food" img={image} />
+    </>
+  )
+}
+
+export default Order
--- src/pages/Shared/PageCover/PageCover.jsx
+++ src/pages/Shared/PageCover/PageCover.jsx
@@ -8,9 +8,9 @@ const PageCover = ({ img, title }) => {
       strength={-200}
     >
       <section
-        className="h-[800px] grid place-items-center">
+        className="h-[700px] grid place-items-center">
           <div className="w-9/12 p-10 md:p-20 bg-black/50 text-neutral-content text-center">
-            <h2 className="text-4xl md:text-7xl font-bold mb-5">{title}</h2>
+            <h2 className="text-4xl md:text-7xl font-bold mb-5 uppercase">{title}</h2>
             <p className="text-xl md:text-2xl">Would you like to try a dish?</p>
           </div>
       </section>
