e91949223c671545b5bb5e5cc50681f099805fc3
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-18 23:39:56 +0600

    installed react-tabs and applied into order page with custom style

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import image from "../../assets/shop/banner2.jpg";
 import PageCover from "../Shared/PageCover/PageCover";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const Order = () => {
-  return (
-    <>
-        <PageCover title="Order Food" img={image} />
-    </>
-  )
+    const [tabIndex, setTabIndex] = useState(0);
+    return (
+        <>
+            <PageCover title="Order Food" img={image} />
+            <section className="pt-20">
+                <div className="container mx-auto flex justify-center">
+                    <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                        <TabList>
+                            <Tab>Salad</Tab>
+                            <Tab>Pizza</Tab>
+                            <Tab>Soups</Tab>
+                            <Tab>Desserts</Tab>
+                            <Tab>Drinks</Tab>
+                        </TabList>
+                        <TabPanel></TabPanel>
+                        <TabPanel></TabPanel>
+                        <TabPanel></TabPanel>
+                        <TabPanel></TabPanel>
+                        <TabPanel></TabPanel>
+                    </Tabs>
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default Order
