e2826fc8fa6fe4105c2b65f755f811a5c8572268
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-19 08:50:20 +0600

    displayed each category items in each category tab in food-order page.

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,39 +1,61 @@
 import { useState } from "react";
 import image from "../../assets/shop/banner2.jpg";
 import PageCover from "../Shared/PageCover/PageCover";
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 import { useParams } from "react-router-dom";
+import useMenu from "../../hooks/useMenu";
+import OrderTab from "./OrderTab";
 
 const Order = () => {
-    const menuItems = ["salad", "pizza", "soup", 'dessert', 'drinks']
-    const { category } = useParams();
-    const initialValue = menuItems.indexOf(category);
-    const [tabIndex, setTabIndex] = useState(initialValue);
+  const menuItems = ["salad", "pizza", "soup", "dessert", "drinks"];
+  const { category } = useParams();
+  const initialValue = menuItems.indexOf(category);
+  const [tabIndex, setTabIndex] = useState(initialValue);
 
-    return (
-        <>
-            <PageCover title="Order Food" img={image} />
-            <section className="pt-20">
-                <div className="container mx-auto flex justify-center">
-                    <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-                        <TabList>
-                            <Tab>Salad</Tab>
-                            <Tab>Pizza</Tab>
-                            <Tab>Soups</Tab>
-                            <Tab>Desserts</Tab>
-                            <Tab>Drinks</Tab>
-                        </TabList>
-                        <TabPanel></TabPanel>
-                        <TabPanel></TabPanel>
-                        <TabPanel></TabPanel>
-                        <TabPanel></TabPanel>
-                        <TabPanel></TabPanel>
-                    </Tabs>
-                </div>
-            </section>
-        </>
-    )
-}
+  const [menu] = useMenu();
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const soup = menu.filter((item) => item.category === "soup");
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const drinks = menu.filter((item) => item.category === "drinks");
 
-export default Order
+  return (
+    <>
+      <PageCover title="Order Food" img={image} />
+      <section className="pt-20">
+        <div className="container mx-auto flex justify-center">
+          <Tabs
+            selectedIndex={tabIndex}
+            onSelect={(index) => setTabIndex(index)}
+          >
+            <TabList>
+              <Tab>Salad</Tab>
+              <Tab>Pizza</Tab>
+              <Tab>Soups</Tab>
+              <Tab>Desserts</Tab>
+              <Tab>Drinks</Tab>
+            </TabList>
+            <TabPanel>
+              <OrderTab items={salad} />
+            </TabPanel>
+            <TabPanel>
+              <OrderTab items={pizza} />
+            </TabPanel>
+            <TabPanel>
+              <OrderTab items={soup} />
+            </TabPanel>
+            <TabPanel>
+              <OrderTab items={dessert} />
+            </TabPanel>
+            <TabPanel>
+              <OrderTab items={drinks} />
+            </TabPanel>
+          </Tabs>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Order;
