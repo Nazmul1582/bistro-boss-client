6c910a68e8ed4d9e4f424707f2d8e0635704093c
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-20 06:49:56 +0600

    created createUser, login, logout functionality in AuthContext

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,14 +1,45 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 
 export const AuthContext = createContext(null);
+const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true)
 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const login = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    } 
+
+    const logout = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
     const authInfo = {
         user,
-        loading
+        loading,
+        createUser,
+        login,
+        logout
     }
 
     return (
