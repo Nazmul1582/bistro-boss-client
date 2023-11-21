2b2b79811c1fc17fe55d457aaa0fc230669c8b1f
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-21 10:53:02 +0600

    added price in every food card

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,10 +1,11 @@
 const FoodCard = ({item}) => {
-    const {image, name, recipe} = item
+    const {image, name, recipe, price} = item
     return (
         <div className="card bg-base-100 shadow-xl">
         <figure>
           <img className="h-96 w-full object-cover" src={image} alt="food image" />
         </figure>
+        <p className="text-white bg-black px-5 py-2 rounded-md absolute top-5 right-5">${price}</p>
         <div className="card-body items-center">
           <h2 className="card-title">{name}</h2>
           <p className="text-center">{recipe}</p>
